e70d682a48f87f42cccf08eeb0e88a1874868654
Princely Onaifo <itsswaely@gmail.com>
2018-12-04 06:39:54 -0500

    game file

---
+++ SleepyShooterZzz/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target; // player
+    public float pace = 5f; // a lil lag to make camera movement smoother..
+
+    Vector3 offset; // distance between camera and player
+
+    void Start()
+    {
+        offset = transform.position - target.position; // initial camera player distance
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset; // variable that maintain initial relative position from player every fixedUpdate
+        // move camera smoothly with Lerp from its current position to the new position 
+        transform.position = Vector3.MoveTowards(transform.position, targetCamPos, pace * Time.deltaTime);
+    }
+}
